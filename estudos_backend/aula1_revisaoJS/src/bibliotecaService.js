27de5aa66ae380dcb21bd575e1d199a4999b58ad
EmilySouza22 <emilyseafonso@gmail.com>
2026-02-12 16:06:15 -0300

    Criando funcionalidade para ver o período de maior saída dos livros e o seu valor financeiro das saídas

--- estudos_backend/aula1_revisaoJS/src/bibliotecaService.js
+++ estudos_backend/aula1_revisaoJS/src/bibliotecaService.js
@@ -1,23 +1,57 @@
 import { pool } from './config.js';
 
 //Buscar um livro específico
-export async function buscarLivroPorId(IdLivro){
-    const [rows] = await pool.query("SELECT * from livros WHERE id=?", 
-        [IdLivro]
-    )
-    return rows[0]
+export async function buscarLivroPorId(IdLivro) {
+	const [rows] = await pool.query('SELECT * from livros WHERE id=?', [IdLivro]);
+	return rows[0];
 }
 
 //Cadastrar um livro na biblioteca
 export async function cadastrarLivro(
-    tituloLivro, 
-    categoriaLivro, 
-    valorUnitarioLivro, 
-    estoqueMinLivro, 
-    estoqueMaxLivro
+	tituloLivro,
+	categoriaLivro,
+	valorUnitarioLivro,
+	estoqueMinLivro,
+	estoqueMaxLivro,
 ) {
-    const [result] = await pool.query("INSERT INTO livros (titulo, categoria, valor_unitario, estoque_minimo, estoque_maximo) VALUES (?, ?, ?, ?, ?)", 
-        [tituloLivro, categoriaLivro, valorUnitarioLivro, estoqueMinLivro, estoqueMaxLivro]
-    )
-    return result.insertId
+	const [result] = await pool.query(
+		'INSERT INTO livros (titulo, categoria, valor_unitario, estoque_minimo, estoque_maximo) VALUES (?, ?, ?, ?, ?)',
+		[
+			tituloLivro,
+			categoriaLivro,
+			valorUnitarioLivro,
+			estoqueMinLivro,
+			estoqueMaxLivro,
+		],
+	);
+	return result.insertId;
+}
+
+// //Livros que possuem maior saída
+export async function livrosMaiorSaidaNoPeriodo(dataInicial, dataFinal) {
+	const [rows] = await pool.query(
+		`SELECT l.id AS livro_id, 
+        l.titulo AS livro, 
+        l.valor_unitario, 
+        m.quantidade_total_saida 
+        FROM livros l 
+        LEFT JOIN 
+        ( SELECT livro_id, SUM(quantidade) AS quantidade_total_saida 
+         FROM movimentacoes 
+         WHERE tipo = 'SAIDA' 
+         AND data_movimentacao 
+         BETWEEN ? AND ? 
+         GROUP BY livro_id ) m ON m.livro_id = l.id 
+         ORDER BY m.quantidade_total_saida DESC`,
+		[dataInicial, dataFinal],
+	);
+	return rows.map((item) => {
+        const quantidade = item.quantidade_total_saida;
+        const valorUnitario = item.valor_unitario;
+        return {
+            livro: item.livro,
+            quantidade_total_saida: quantidade,
+            valor_total_financeiro_saidas: quantidade * valorUnitario
+        }
+    });
 }
--- estudos_backend/aula1_revisaoJS/src/index.js
+++ estudos_backend/aula1_revisaoJS/src/index.js
@@ -1,4 +1,4 @@
-import { buscarLivroPorId, cadastrarLivro } from "./bibliotecaService.js";;
+import { buscarLivroPorId, cadastrarLivro, livrosMaiorSaidaNoPeriodo } from "./bibliotecaService.js";;
 import { pool } from './config.js';
 
 async function main() {
@@ -6,7 +6,9 @@ async function main() {
 
     console.log(`Cadastrando livro...`, await cadastrarLivro('Livro 1', 'Terror', 10.5, 5, 10))
 
-
+    const dataInicial = "2026-01-01 00:00:00";
+    const dataFinal = "2026-12-31 23:59:59";
+    console.log(await livrosMaiorSaidaNoPeriodo(dataInicial, dataFinal))
 }
 
 main().catch(error => 
