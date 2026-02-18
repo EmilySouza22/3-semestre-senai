db10cca19aaec38a167b2bd5e26465d08a63797b
emilysouza22 <emilyseafonso@gmail.com>
2026-02-18 20:28:00 -0300

    Adiciona exemplo de código de promise

---
+++ estudos_backend/aula3_async/promise.js
@@ -0,0 +1,11 @@
+// Exemplo de Promise simulando tempo de espera
+function esperarTempo(ms){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve (`Esperou ${ms} milissegundos`)
+    
+        // Para simular o erro:
+        reject(new Error('Falhou'))}, ms);
+
+    })
+}
+esperarTempo(2000).then(resultado => console.log(resultado)).catch(erro => console.error(error));
