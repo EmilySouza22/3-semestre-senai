12bdf288ea89de3931a2ddc504577be6af543354
emilysouza22 <emilyseafonso@gmail.com>
2026-02-24 19:44:35 -0300

    Construindo base frontend

---
+++ estudos_frontend/aula1/fundamentos/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './layouts/Header/Index'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
