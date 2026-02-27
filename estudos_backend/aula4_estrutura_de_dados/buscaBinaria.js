95b83c425f91ecd314f6adc286794c1986c30047
EmilySouza22 <emilyseafonso@gmail.com>
2026-02-27 00:23:10 -0300

    Adicionando função buscaBinaria

--- estudos_backend/aula4_estrutura_de_dados/buscaBinaria.js
+++ estudos_backend/aula4_estrutura_de_dados/buscaBinaria.js
@@ -1,3 +1,23 @@
 //Ordenar array
 const usuarios = [12, 45, 3, 89, 21, 67, 34, 10];
+
 // Ordenado vai ficar assim [3,10,12,21,34,45,67,89]
+const usuariosOrdenados = usuarios.sort((a, b) => a - b);
+
+function buscaBinaria(arr, valor) {
+  let inicio = 0;
+  let fim = arr.length - 1;
+
+  while (inicio <= fim) {
+    let meio = Math.floor((inicio + fim) / 2);0
+    if (arr[meio] === valor) return meio;
+
+    if (arr[meio] < valor) inicio = meio + 1;
+    else fim = meio - 1;
+  }
+
+  return -1;
+}
+
+console.log(buscaBinaria(usuariosOrdenados, 12));
+//deve retornar 2
