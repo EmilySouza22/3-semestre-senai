08fcd2ed145314b8bbb76ff86a8ce7c029bd2943
EmilySouza22 <emilyseafonso@gmail.com>
2026-03-01 23:53:31 -0300

    Adiciona comentarios e isEmpty() no exercício1.js

--- estudos_backend/aula5_exercicios/exercicio1.js
+++ estudos_backend/aula5_exercicios/exercicio1.js
@@ -1,49 +1,72 @@
 // Exercício 1 — Simulador de Atendimento
 class Queue {
-    constructor(){
-        this.array = new Array()
-        this.inicio = 0
-        this.final = 0
-        this.tamanho = 0
-    }
+	constructor() {
+		this.array = new Array();
+		this.inicio = 0;
+		this.final = 0;
+		this.tamanho = 0;
+	}
 
-    enqueue(value){
-        this.array[this.final] = value
-        this.final = this.final + 1
-        this.tamanho = this.tamanho + 1
-    }
+	enqueue(value) {
+		this.array[this.final] = value;
+		this.final = this.final + 1;
+		this.tamanho = this.tamanho + 1;
+	}
 
-    dequeue(){
-        if(this.tamanho === 0) return "fila vazia"
-        const removido = this.array[this.inicio]
-        this.array[this.inicio] = undefined
-        this.inicio = this.inicio + 1
-        this.tamanho = this.tamanho - 1
-        return removido;
-    }
+    //caso tamanho === 0 vai retornar fila vazia se não vai remover o primeiro valor da fila
+	dequeue() {
+		if (this.tamanho === 0) return 'fila vazia';
+		const removido = this.array[this.inicio];
+		this.array[this.inicio] = undefined;
+		this.inicio = this.inicio + 1;
+		this.tamanho = this.tamanho - 1;
+		return removido;
+	}
 
-    peek(){
-        return this.array[this.inicio]
-    }
+    //retorna o primeiro valor da fila
+	peek() {
+		return this.array[this.inicio];
+	}
 
-    mostrarTamanho(){
-        return this.tamanho
-    }
+    //apenas mostra o tamanho da fila
+	mostrarTamanho() {
+		return this.tamanho;
+	}
+
+    //verifica se a fila ta vazia retornando true ou false
+	isEmpty() {
+		return this.tamanho === 0;
+	}
 }
 
 const fila = new Queue();
 
-fila.enqueue("Emily");
-fila.enqueue("Alice");
-fila.enqueue("Emilie");
+//adiciona pessoas na fila
+fila.enqueue('Emily');
+fila.enqueue('Alice');
+fila.enqueue('Emilie');
 
-console.log(fila.dequeue())
-console.log(fila.dequeue())
-console.log(fila.dequeue())
-console.log(fila.dequeue())
-console.log(fila.mostrarTamanho())
+//tira as pessoas da fila, deixando vazia
+console.log(fila.dequeue());
+console.log(fila.dequeue());
+console.log(fila.dequeue());
+console.log(fila.dequeue());
 
-fila.enqueue("Emilie")
-console.log(fila.mostrarTamanho())
+//retorna 0 pq não tem ngm na fila
+console.log(fila.mostrarTamanho());
 
-console.log(fila.peek())
+//retorna true, está vazia
+
+console.log(fila.isEmpty());
+
+//adiciona pessoa
+fila.enqueue('Emilie');
+
+//agora retorna 1
+console.log(fila.mostrarTamanho());
+
+//retorna 'Emilie'
+console.log(fila.peek());
+
+//retorna false, não está vazia
+console.log(fila.isEmpty());
