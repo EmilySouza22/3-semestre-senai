c717d4cf9f19062eec90b894db1fb4883d59c517
emilysouza22 <emilyseafonso@gmail.com>
2026-03-04 20:23:42 -0300

    Arrumando exercicio validadorParenteses()

--- estudos_backend/aula5_exercicios/exercicio4.js
+++ estudos_backend/aula5_exercicios/exercicio4.js
@@ -10,9 +10,9 @@ class ValidadorParenteses {
 		const pilha = [];
 
 		for (let char of expressao) {
-			if (char === '(') {
+			if (char === '(' || char === '[' || char === '{' ) {
 				pilha.push(char);
-			} else if (char === ')') {
+			} else if (char === ')' || char === ']' || char === '}' ) {
 				if (pilha.length === 0) {
 					return false;
 				}
@@ -28,3 +28,35 @@ const validador = new ValidadorParenteses();
 console.log(validador.validar('((a+b)*c)'));
 console.log(validador.validar('(a + b'));
 console.log(validador.validar('a + b)'));
+
+
+// Outra forma
+
+function validarBalanceamento(expressao) {
+	const pilha = []
+	const pares = {
+		")" : "(",
+		"]" : "[",
+		"}" : "}"
+	}
+
+	const quemAbre = new Set(["(", "[", "{"])
+
+	for(const caractere of expressao) {
+		if(quemAbre.has(caractere)) {
+			pilha.push(caractere);
+		} else if (caractere in pares) {
+			if(pilha.length === 0) {
+				return false;
+			}
+			const topo = pilha.pop()
+			if(topo !== pares[caractere]){
+				return false;
+			}
+		}
+	}
+
+	return pilha.length == 0;
+}
+
+console.log(validarBalanceamento('((a+b)*c)'))
