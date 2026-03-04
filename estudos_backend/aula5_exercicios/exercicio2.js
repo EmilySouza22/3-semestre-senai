bd9baaeaa39ffa2c894150fdb9195e519b7c5c38
EmilySouza22 <emilyseafonso@gmail.com>
2026-03-04 15:56:46 -0300

    Ajustando exercício de fila com prioridade e adicionando outro exemplo de fila

--- estudos_backend/aula5_exercicios/exercicio2.js
+++ estudos_backend/aula5_exercicios/exercicio2.js
@@ -1,33 +1,32 @@
-// Exercício 2 — Fila com Prioridade Simples
+// Exercício 2 — Fila com Prioridade
 
-class Queue{
+class Queue {
+	constructor() {
+		this.array = new Array();
+		this.inicio = 0;
+		this.final = 0;
+		this.tamanho = 0;
+	}
 
-    constructor(){
-        this.array = new Array();
-        this.inicio = 0;
-        this.final = 0;
-        this.tamanho = 0;
-    }
+	enqueue(value) {
+		if (value.isElderly) {
+			let contador = 0;
+			while (contador < this.array.length) {
+				if (this.array[contador] && this.array[contador].isElderly) {
+                    contador++;
+				} else {
+					break;
+				}
+			}
+			this.array.splice(contador, 0, value);
+		} else {
+			this.array.push(value);
+		}
 
-    enqueue(value){
-        if(value.isElderly){
-            let contador = 0
-                while(contador < this.array.length){
-                    if(this.array[contador] && this.array[contador].isElderly) {
-                        contador++
-                    } else {
-                        break
-                    }
-                } 
-                this.array.splice(contador, 0 , value)
-            }else{
-            this.array.push(value)
-        }
+		this.tamanho++;
+	}
 
-        this.tamanho++;
-    }
-
-    dequeue() {
+	dequeue() {
 		if (this.tamanho === 0) return 'fila vazia';
 		const removido = this.array[this.inicio];
 		this.array[this.inicio] = undefined;
@@ -36,11 +35,11 @@ class Queue{
 		return removido;
 	}
 
-    peek() {
+	peek() {
 		return this.array[this.inicio];
 	}
 
-    mostrarTamanho() {
+	mostrarTamanho() {
 		return this.tamanho;
 	}
 
@@ -48,3 +47,63 @@ class Queue{
 		return this.tamanho === 0;
 	}
 }
+
+const queue = new Queue();
+
+queue.enqueue({name: 'Emily', isElderly: false});
+queue.enqueue({name: 'João', isElderly: true});
+queue.enqueue({name: 'Maria', isElderly: false});
+queue.enqueue({name: 'Gabriel', isElderly: true});
+
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+
+
+//Outra forma de Fila com prioridade
+class PriorityQueue {
+	constructor() {
+        this.items = [];
+    }
+
+    //menor número = maior prioridade
+    enqueue(value, priority) {
+        const newItem = {value, priority}
+        let added = false;
+        for (let i = 0; i < this.items.length; i++) {
+            if (priority < this.items[i].priority) {
+                this.items.splice(i, 0, newItem);
+                added = true;
+                break
+            }
+        }
+        
+        if(!added) {
+            this.items.push(newItem)
+        }
+
+    }
+
+    dequeue() {
+        return this.items.shift();
+    }
+
+    peek() {
+        return this.items[0];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+}
+
+const queue2 = new PriorityQueue();
+
+queue2.enqueue('Low priority task', 5);
+queue2.enqueue('High priority task', 1);
+queue2.enqueue('Medium priority task', 3);
+
+console.log(queue2.dequeue());
+console.log(queue2.dequeue());
+console.log(queue2.dequeue());
