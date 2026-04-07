1c0e3e23b11e2f36e233b6797eed8f9f2dada45d
EmilySouza22 <emilyseafonso@gmail.com>
2026-04-06 23:59:44 -0300

    Alterações no multi-pages

--- estudos_frontend/aula4/multi-pages/src/main.jsx
+++ estudos_frontend/aula4/multi-pages/src/main.jsx
@@ -1,54 +1,60 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 // import App from './App.jsx'
 
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-import { Home } from "./pages/Home"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home } from './pages/Home';
 import { Sobre } from './pages/Sobre';
 import { AuthProvider } from './context/AuthContext';
 import { Main } from './layouts/Main';
-import { Login }  from './pages/Login';
+import { Login } from './pages/Login';
 import Blog from './pages/Blog';
+import { Authors } from './pages/Author';
+import AuthorsDetail from './pages/Author/AuthorDetail';
+import PostDetail from './pages/Blog/PostDetail';
 
-const router = createBrowserRouter([
-  {
-    element: <Main />,
-    children:[
-      {
-        path: "/", element: <Home />
-      },
-      {
-        path: "sobre", element: <Sobre />
-      },
-      {
-        path: "blog", element: <Blog />
-      },
-      {
-        path: "post/:id", element: <Blog />
-      }
-    ]
-  },
-  {
-    path:"login",
-    element:<Login/>
-  }
-  // {
-  //   path: "/",
-  //   element: <Home/>,
-  // },
-  // {
-  //   path: "/sobre",
-  //   element: <Sobre/>,
-  // },
+export const router = createBrowserRouter([
+	{
+		element: <Main />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+			{
+				path: 'sobre',
+				element: <Sobre />,
+			},
+			{
+				path: 'blog',
+				element: <Blog />,
+			},
+			{
+				path: 'autores',
+				element: <Authors />,
+			},
+			{
+				path: 'post/:id',
+				element: <PostDetail />,
+			},
+			{
+				path: 'autores/:id',
+				element: <AuthorsDetail />,
+			},
+		],
+	},
+	{
+		path: 'login',
+		element: <Login />,
+	},
 ]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-    {/* <App /> */}
-  </StrictMode>,
-)
+	<StrictMode>
+		<AuthProvider>
+			<RouterProvider router={router} />
+		</AuthProvider>
+		{/* <App /> */}
+	</StrictMode>,
+);
